7cd3f413c5f770236388ff44419495564135d84f
Devendra-sagar <156461175+Devendra-sagar@users.noreply.github.com>
2024-01-15 13:47:28 -0500

    Deleting and adding statement to Script 1 and 2
    Deleting 1 statement from script 1 and adding 1 to script 2.

--- script2.py
+++ script2.py
@@ -1,3 +1,4 @@
 #lines from zen of python
 print("Explicit is better than implicit.")
 print("There should be one-- and preferably only one --obvious way to do it.")
+print("In the face of ambiguity, refuse the temptation to guess.")
